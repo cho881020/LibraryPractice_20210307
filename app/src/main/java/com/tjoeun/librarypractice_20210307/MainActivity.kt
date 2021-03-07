da5839824ecdebb7b7b525d03d7b372789cdecb5
조경진 <cho881020@gmail.com>
2021-03-07 16:18:46 +0900

    실제 권한 체크 실행

--- app/src/main/java/com/tjoeun/librarypractice_20210307/MainActivity.kt
+++ app/src/main/java/com/tjoeun/librarypractice_20210307/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.tjoeun.librarypractice_20210307
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -41,6 +43,11 @@ class MainActivity : BaseActivity() {
 
 //            2. 1에서 만든 [행동 방침]을 가지고 => 실제 권한 확인.
 
+            TedPermission.with(mContext)
+                .setPermissionListener(pl)
+                .setDeniedMessage("권한을 거부하면 연결 불가합니다. [설정] > [권한]에서 세팅해주세요.")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
 
         }
 
